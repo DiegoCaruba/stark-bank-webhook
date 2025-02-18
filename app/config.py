1a38f14bbc2af275e19e1a590bc7b5bdf8a2e474
Diego Caruba <diego.caruba@suridata.com.br>
2025-02-17 21:12:51 -0300

    chore(config): move sensitive data to .env

--- app/config.py
+++ app/config.py
@@ -1,17 +1,20 @@
-environment = "sandbox"
-project_id = "6311638751772672"
+import os
+from dotenv import load_dotenv
 
-private_key = "privateKey.pem"
+load_dotenv()
 
-webhook_url = (
-    "https://9f9e-179-101-239-172.ngrok-free.app/webhook"
-)
+environment = os.getenv("STARKBANK_ENVIRONMENT")
+project_id = os.getenv("SANDBOX_PROJECT_ID")
+
+private_key = os.getenv("PRIVATE_KEY_PATH")
+
+webhook_url = os.getenv("WEBHOOK_URL")
 
 destination_account = {
-    "tax_id": "20.018.183/0001-80",
-    "name": "Stark Bank S.A.",
-    "bank_code": "20018183",
-    "branch_code": "0001",
-    "account_number": "6341320293482496",
-    "account_type": "payment",
+    "tax_id": os.getenv("ACCOUNT_TAX_ID"),
+    "name": os.getenv("ACCOUNT_NAME"),
+    "bank_code": os.getenv("ACCOUNT_BANK_CODE"),
+    "branch_code": os.getenv("ACCOUNT_BRANCH_CODE"),
+    "account_number": os.getenv("ACCOUNT_NUMBER"),
+    "account_type": os.getenv("ACCOUNT_TYPE"),
 }
